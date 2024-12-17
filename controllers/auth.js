725c0b697b8618ab5fbcfea1e912764ed3a7a4ef
Gonzalo Torres <gonzalotorreshernandez51@gmail.com>
2024-12-17 12:04:33 -0300

    Update controller / model auth

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,5 @@
 const { response } = require('express');
-const Usuario = require('../Models/Usuario');
+const Usuario = require('../models/Usuario');
 const bcrypt = require('bcryptjs');
 const { generarJWT } = require('../helpers/jwt');
 
