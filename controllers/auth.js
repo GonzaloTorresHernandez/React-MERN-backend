d5bea925d1d8193fc7278a9c4170f5708eb9a5e9
Gonzalo Torres <gonzalotorreshernandez51@gmail.com>
2025-01-03 16:51:48 -0300

    Ultima modificacion en auth.js/controllers

--- controllers/auth.js
+++ controllers/auth.js
@@ -103,7 +103,7 @@ const revalidarToken = async (req, res = response) => {
     const token = await generarJWT(uid, name);
 
 
-    res.status(200).json({ ok: true, token });
+    res.status(200).json({ ok: true, uid, name, token });
 
 };
 
